e98a666bad79033208b224cd4bf95cf88bc1289b
yrungr <60988124+yrungr@users.noreply.github.com>
2020-03-15 21:01:22 +0000

    Update backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -8,5 +8,5 @@ sqlite3 world/map.sqlite ".backup world/map.sqlite.backup"
 sqlite3 world/players.sqlite ".backup world/players.sqlite.backup"
 
 # backup everything except the live databases
-name=$(date '+%Y-%m-%d_%H:%M:%S')
+name=$(date '+%Y%m%d_%H%M%S')
 tar -zcvf /var/www/html/minetest/$name.tar.gz --exclude='auth.sqlite' --exclude='map.sqlite' --exclude='players.sqlite' world

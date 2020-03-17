56068ddb6d94d373ea1d32df7aded53155d29cc7
yrungr <60988124+yrungr@users.noreply.github.com>
2020-03-17 15:33:36 +0000

    Update backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 cd ~/.minetest/worlds/mineclone2
-backupfile="/var/www/html/mineclone2_backups/map_$(date '+%Y%m%d_%H%M%S').sqlite
+datetime=$(date '+%Y%m%d_%H%M%S')
+backupfile="/var/www/html/mineclone2_backups/map_$datetime.sqlite"
 sqlite3 mineclone2/map.sqlite ".backup $backupfile"

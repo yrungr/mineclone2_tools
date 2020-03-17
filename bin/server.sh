415fe2df7dcd79fc4b6427d9e262fdbffcec6e59
yrungr <60988124+yrungr@users.noreply.github.com>
2020-03-17 21:09:33 +0000

    Update server.sh

--- bin/server.sh
+++ bin/server.sh
@@ -4,4 +4,4 @@ executable="/home/yrungr/minetest-stable/bin/minetestserver"
 configfile="/home/yrungr/minetest_mineclone2/mineclone2.conf"
 logfile="/var/www/html/mineclone2.txt"
 
-$executable --config $configfile --logfile $logfile
+$executable --config $configfile --logfile $logfile --world mineclone2

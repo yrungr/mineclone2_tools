3d0e383162dce691f160ac5aee3d4282ccdb8bea
yrungr <60988124+yrungr@users.noreply.github.com>
2020-03-17 15:30:14 +0000

    Update backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-cd ~/.minetest/worlds
-
-# make copies of the live databases
-sqlite3 mineclone2/auth.sqlite ".backup mineclone2/auth.sqlite.backup"
-sqlite3 mineclone2/map.sqlite ".backup mineclone2/map.sqlite.backup"
-sqlite3 mineclone2/players.sqlite ".backup mineclone2/players.sqlite.backup"
-
-# backup everything except the live databases
-name=$(date '+%Y%m%d_%H%M%S')
-tar -zcvf ~/minetest_backups/$name.tar.gz --exclude='auth.sqlite' --exclude='map.sqlite' --exclude='players.sqlite' mineclone2
+cd ~/.minetest/worlds/mineclone2
+backupfile=map_$(date '+%Y%m%d_%H%M%S').sqlite
+sqlite3 mineclone2/map.sqlite ".backup $backupfile"

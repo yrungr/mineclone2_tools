5851f7e8a0531fe9335037ef34208d09880fc722
yrungr <60988124+yrungr@users.noreply.github.com>
2020-03-21 20:48:11 +0000

    Update backup.sh

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 
 datetime=$(date '+%Y%m%d_%H%M%S')
-cd ~/.minetest/worlds
+cd /home/mineclone2/.minetest/worlds
 
 # backup directory
 if [ -d "backup" ]; then
-  rm -rf backup
-  mkdir backup
+	rm -rf backup
 fi
+	mkdir backup
 
 # copy everything except the sqlite files
 rsync -avr --exclude='auth.sqlite' --exclude='players.sqlite' --exclude='map.sqlite' mineclone2/ backup
 
 # copy the sqlite files
-sqlite3 mineclone2/auth.sqlite ".backup backup/auth.sqlite"
-sqlite3 mineclone2/map.sqlite ".backup backup/map.sqlite"
-sqlite3 mineclone2/players.sqlite ".backup backup/players.sqlite"
+while [ ! -f backup/auth.sqlite ]
+do
+	sqlite3 mineclone2/auth.sqlite ".backup backup/auth.sqlite"
+done
+while [ ! -f backup/map.sqlite ]
+do
+	sqlite3 mineclone2/map.sqlite ".backup backup/map.sqlite"
+done
+while [ ! -f backup/players.sqlite ]
+do
+	sqlite3 mineclone2/players.sqlite ".backup backup/players.sqlite"
+done
 
 # compress
-tar -zcvf ~/minetest_backups/$datetime.tar.gz --exclude='auth.sqlite' backup
+tar -zcvf /home/mineclone2/minetest_backups/$datetime.tar.gz backup
 
-# copy just the map sqlite to http://178.62.65.106/mineclone2_backups/
-backupfile="/var/www/html/mineclone2_backups/"$datetime"_map.sqlite"
-cp backup/map.sqlite $backupfile
+# clean up
+rm -rf backup
